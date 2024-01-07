8c1072e2d5e00505aa71cfc557eb226d1ada5c44
ariefsulthanul468 <ariefsulthanul468@gmail.com>
2024-01-07 15:51:11 +0530

    Footer changed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import React from "react";
 import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
+const year = new Date().getFullYear();
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 JSM Headphones All rights reserverd</p>
+      <p>{year} Arief Headphones All rights reserverd</p>
       <p className="icons">
         <Link href="https://www.instagram.com/">
           <AiFillInstagram />
